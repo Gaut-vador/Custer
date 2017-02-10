6641b1cc0a256b562357584bdd3f2ff3f7b8a6a3
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2017-02-10 15:30:04 +0100

    Dell

--- webserver/main.c
+++ webserver/main.c
@@ -1,38 +1,38 @@
 #include "header.h"
 
 void traitement_signal(int sig){
-  	printf("Traitement du signal reçu\n");
-  	printf ("Signal %d reçu \n",sig);
+  printf("Traitement du signal reçu\n");
+  printf ("Signal %d reçu \n",sig);
 }
 
 void initialiser_signaux (void) {
-  	printf("Initialiser Signaux\n");
-
-  	if (signal(SIGPIPE,SIG_IGN) == SIG_ERR){
-    	perror ("signal SIGPIPE");
-  	}
+  printf("Initialiser Signaux\n");
   
-  	struct sigaction sa;
-  	sa.sa_handler = traitement_signal;
-  	sigemptyset(&sa.sa_mask);
-  	sa.sa_flags = SA_RESTART ;
-  	if (sigaction(SIGCHLD,&sa,NULL) == -1){
-    	perror("sigaction(SIGCHLD)");
-  	}
+  if (signal(SIGPIPE,SIG_IGN) == SIG_ERR){
+    perror ("signal SIGPIPE");
+  }
+  
+  struct sigaction sa;
+  sa.sa_handler = traitement_signal;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART ;
+  if (sigaction(SIGCHLD,&sa,NULL) == -1){
+    perror("sigaction(SIGCHLD)");
+  }
 }
 
 
-int main ( int argc , char ** argv ){
-  	printf("Loading Cavalry... ");
-  	
-  	//initialiser_signaux();
-  	int socket_server = creer_serveur(8080);
-  	if(socket_server)
-    	printf("Let's make America great !\n");
-  	else
-    	printf("Apaches won !\n");
-  	while(socket_server){
-    	accepter_connexion(socket_server);
-  	}
-  	return 0;
+int main(int argc, char ** argv){
+  printf("Loading Cavalry... ");
+  
+  initialiser_signaux();
+  int socket_server = creer_serveur(8084);
+  if(socket_server)
+    printf("Let's make America great !\n");
+  else
+    printf("Apaches won !\n");
+
+  accepter_connexion(socket_server);
+
+  return 0;
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,42 +1,44 @@
 #include "header.h"
 
 void accepter_connexion(int socket_serveur){
-  	int socket_client ;
-  	socket_client = accept((socket_serveur), NULL, NULL);
 
-  	if (socket_client == -1){
-    	perror("accept socket_client");
-    	/* traitement d ’ erreur */
-    	exit(1); // NE PAS ENLEVER le exit gere le lancement d'un autre serveur sur le meme port
-  	}
-  
-  	/*On peut maintenant dialoguer avec le client */
-  	const char * message_bienvenue = "\n\nBonjour, bienvenue sur le serveur Custer!\nSur ce serveur nous ne traitons pas nos clients comme des Apaches!\nGeorge Armstrong Custer est un général de cavalerie américain\n(5 décembre 1839 New Rumley, Ohio - 25 juin 1876, Montana).\n\nIl est célèbre pour ses exploits durant la guerre de Sécession et sa défaite lors de la bataille de Little Bighorn face à une coalition de tribus indiennes.\nIl est une des principales figures américaines des guerres indiennes du xixe siècle.\n\n";
+  const char * message_bienvenue = "\n\nBonjour, bienvenue sur le serveur Custer!\nSur ce serveur nous ne traitons pas nos clients comme des Apaches!\nGeorge Armstrong Custer est un général de cavalerie américain\n(5 décembre 1839 New Rumley, Ohio - 25 juin 1876, Montana).\n\nIl est célèbre pour ses exploits durant la guerre de Sécession et sa défaite lors de la bataille de Little Bighorn face à une coalition de tribus indiennes.\nIl est une des principales figures américaines des guerres indiennes du xixe siècle.\n\n";
   	//sleep(1);
   
-  	pid_t pid = 0;
-  	if((pid = fork()) == -1)
-    	perror("fork");
-  	if(pid != 0) {
-    	close(socket_client);
-  	} else {
-    	write (socket_client, message_bienvenue, strlen(message_bienvenue));
-
-    	char message[50];
-    	while(socket_client){
-      		sleep(1);
-      		int length = read(socket_client, message, 50);
-      		if(length > 0){
-				write(socket_client, message, length);
-      		}
-    	}
-  	}
-  	int status; 
-  	waitpid(pid,&status, WNOHANG);
+  int socket_client ;
+  while(1) {
+    socket_client = accept((socket_serveur), NULL, NULL);
+    if (socket_client == -1){
+      perror("accept socket_client");
+      exit(1); // NE PAS ENLEVER le exit gere le lancement d'un autre serveur sur le meme port
+    }
+    
+    pid_t pid = 0;
+    if((pid = fork()) == -1)
+      perror("fork");
+    if(pid != 0) {
+      close(socket_client);
+    } else {
+      write (socket_client, message_bienvenue, strlen(message_bienvenue));
+      if(fork())
+	execlp("cat","cat ../data/custer.ascii");
+      else
+	return;
+      char message[50];
+      while(socket_client){
+	sleep(1);
+	int length = read(socket_client, message, 50);
+	if(length > 0){
+	  write(socket_client, message, length);
+	}
+      }
+    }
+  }
 }
 
 int creer_serveur (int port) {  
   //creation de la socket
+  port = 8080;
   int socket_serveur ;
   socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
   if ( socket_serveur == -1){
@@ -45,11 +47,10 @@ int creer_serveur (int port) {
   }
   
   struct sockaddr_in saddr ;
-  
   saddr.sin_family = AF_INET ; /* Socket ipv4 */
   saddr.sin_port = htons(port); /* Port d ’écoute */
   saddr.sin_addr.s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
-
+  
   int optval = 1;
   if ( setsockopt (socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
     perror ( " Can not set SO_REUSEADDR option " );
