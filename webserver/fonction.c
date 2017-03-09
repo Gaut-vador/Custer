968b1dc5a32ebda5a701a578d69aa83717945a2f
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2017-03-09 10:06:33 +0100

    ça marche

--- webserver/fonction.c
+++ webserver/fonction.c
@@ -9,3 +9,27 @@ int nbMots(char *chaine) {
       ;
   return compteur;
 }
+
+char** mots(char *str) {
+  char *chaine = str;
+  char *stRez = malloc(sizeof(char)*50);
+  char **rez = malloc(sizeof(stRez)*10);
+  int cpt = 0;
+  int stCpt = 0;
+  int i;
+  
+  for(i=0;chaine[i]!='\0';i++) {
+    while(chaine[i] == ' ' && chaine[i+1] == ' ')
+      i++;
+    if(chaine[i]!=' ' && chaine[i] != '\n') {
+      stRez[stCpt ++] = chaine[i];
+    }else {
+      stCpt = 0;
+      rez[cpt++] = stRez;
+      printf("%s",stRez);
+      memset(stRez,0,sizeof(stRez));
+      printf("\n");
+    }
+  }
+  return rez;
+}
